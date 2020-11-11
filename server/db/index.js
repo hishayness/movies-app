ed8e67470dac4bd2d7c26a3cdfe5d8f32c6f142e
Ed Lin <eddie.lin@gmail.com>
2020-11-10 20:06:59 -1000

    hardcode

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-	.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cinema', { useNewUrlParser: true })
+	.connect('mongodb+srv://admin:admin123@cluster0.o4fgx.mongodb.net/cinema', { useNewUrlParser: true })
 	.catch(e => {
 		console.error('Connection error', e.message);
 	});
