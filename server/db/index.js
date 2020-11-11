d9bac954aed13faa106dd364c9a052320b2d39b5
Ed Lin <eddie.lin@gmail.com>
2020-11-10 20:21:11 -1000

    update

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-	.connect('mongodb+srv://admin:admin123@cluster0.o4fgx.mongodb.net/cinema', { useNewUrlParser: true })
+	.connect('mongodb+srv://admin:admin123@cluster0.o4fgx.mongodb.net/cinema?retryWrites=true&w=majority', { useNewUrlParser: true })
 	.catch(e => {
 		console.error('Connection error', e.message);
 	});
